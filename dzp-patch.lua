e6f3379484ca249c7e502926ee61b39e2a1414f7
dzp <danzipao@gmail.com>
2019-09-05 15:34:12 +0800

    Feat: 为 require 增加计时并显示，及增加三个函数: Run/GetVar/printf

--- dzp-patch.lua
+++ dzp-patch.lua
@@ -1,11 +1,30 @@
+if utf8 == nil then
+    utf8 = {}
+    utf8.charpattern = "[\0-\127\194-\244][\128-\191]*"
+end
+
 -- 字符串到拼音（首字母）
 function string2pinyin(s)
      for p, c in utf8.codes(s) do
      end
 end
 
-if utf8 == nil then
-    utf8 = {}
-    -- utf8.charpattern = "[\0-\x7F\xC2-\xF4][\x80-\xBF]*"
-    utf8.charpattern = "[\0-\127\194-\244][\128-\191]*"
+printf = function(s,...)
+    return Print(s:format(...))
+end
+
+lua_require = require
+require = function (name)
+    t1 = os.clock()
+    ret = lua_require(name)
+    t2 = os.clock()
+    elapse = t2 - t1
+    printf("消耗时间 %.3fs 以加载 %s", elapse, name)
+    return ret
+end
+
+Run=exe
+
+GetVar = function (name)
+    return var.name
 end
